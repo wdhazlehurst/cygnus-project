ada597d5cb2e3126c10d8ad3aafd2c2541fb0e8a
Will Hazlehurst <willhazlehurst01@gmail.com>
2023-09-05 00:17:42 -0400

    Plugin compiles successfully, now to branch

--- src/main/java/io/github/wdhazleh/plugin/CygnusPlugin.java
+++ src/main/java/io/github/wdhazleh/plugin/CygnusPlugin.java
@@ -1,14 +1,52 @@
 package io.github.wdhazleh.plugin;
 
+import org.bukkit.command.Command;
+import org.bukkit.command.CommandSender;
+import org.bukkit.entity.Player;
 import org.bukkit.plugin.java.JavaPlugin;
 
+import net.md_5.bungee.api.ChatColor;
+
 public class CygnusPlugin extends JavaPlugin {
+	/**
+	 * Plugin startup Logic
+	 */
     @Override
     public void onEnable() {
-        getLogger().info("onEnable is called!");
+        //getLogger().info("onEnable is called!");
+        System.out.println("Cygnus plugin has launched successfully!");
     }
+    /**
+     * Plugin shutdown Logic
+     */
     @Override
     public void onDisable() {
-        getLogger().info("onDisable is called!");
+        //getLogger().info("onDisable is called!");
+        System.out.println("Cygnus plugin disabled.");
+    }
+    
+    @Override
+    public boolean onCommand(CommandSender sender, Command command, String label, String[] args) {
+    	
+    	if(label.equalsIgnoreCase("cygnus")) {
+    		// Handle console input
+    		if(!(sender instanceof Player)) {
+    			sender.sendMessage(ChatColor.RED + "You need to be a player to run this command");
+    			return true;
+    		} else {
+        		if(args[0].equalsIgnoreCase("sort")) {
+        			if(args.length == 2) {
+        				sender.sendMessage("Sorting using the " + args[1] + " comparator");
+        			} else if(args[2].equals("reverse")) {
+        				sender.sendMessage("Sorting using the " + args[1] + "comparator in reverse");
+        			} else {
+        				sender.sendMessage(ChatColor.RED + "Error.");
+        			}
+        		}
+        		return true;
+    		}
+    	}
+    	
+    	return false;
     }
 }
